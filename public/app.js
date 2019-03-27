98b9ab55bc26f62a406c0173ae4ae57962e1a720
Jonas Schett <jonas.schett@gmail.com>
2019-03-27 10:25:26 +0000

    Updated way database is written to

--- public/app.js
+++ public/app.js
@@ -20,12 +20,47 @@ document.addEventListener("DOMContentLoaded", event => {
 
 });
 
+const form = document.querySelector("#addPlayerForm")
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    firebase.firestore().collection("Players").doc(form.name.value).set({
+        name: form.name.value,
+        points: 1500,
+        wins: 0,
+        losses: 0,
+        currentStreak: 0,
+        maxStreak: 0,
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+
+    form.name.value = '';
+});
+
 function googleLogin(){
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(result =>{
+        
         const user = result.user;
-        var dName = user.displayName;
-        document.write("Hello " + dName);
-        console.log(user);
+        console.log(user.displayName);
     }).catch(console.log)
+}
+
+function passwordLogin(){
+    var password = prompt("Enter the password:");
+    firebase.auth().signInWithEmailAndPassword('loginuser@tablefootball.com', password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+}
+
+function addData(){
+
 }
