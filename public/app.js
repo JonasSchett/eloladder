5897681a41f9108b3c7b6f2b26d70af5cfe331d8
Jonas Schett <jonas.schett@gmail.com>
2019-03-27 21:18:31 +0000

    Added listener to changes in the players list and automatic addition to table

--- public/app.js
+++ public/app.js
@@ -1,30 +1,73 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
     console.log(app);
+    const mainTable = document.querySelector("#mainTableBody");
 
     const db = firebase.firestore();
-    const info = db.collection('Players').doc('test');
+    const authVerification = db.collection('information').doc('authVerification');
     // below is a continous stream
-    info.onSnapshot(doc => {
-        const data = doc.data();
-        var a = document.querySelector(".databaseRetrieval");
-        a.textContent = data.title;
+    authVerification.onSnapshot(doc => {
+        var data = doc.data();
+        var connectionStatus = document.querySelector("#connectionStatus");
+        connectionStatus.textContent = data.info;
+        connectionStatus.classList.add("connectionSuccess");
     });
-    // below gets it once
-    // info.get().then(doc => {
-    //     const data = doc.data();
-    //     var a = document.querySelector(".databaseRetrieval");
-    //     a.textContent = data.title;
-    // });
+    db.collection("Players").orderBy("points", "desc").onSnapshot(doc =>{
+        while(mainTable.firstChild){
+            mainTable.removeChild(mainTable.firstChild);
+        }
+        var counter = 1;
+        doc.forEach(function(entry){
+            let div = document.createElement('div');
+            let row = document.createElement('tr');
+            let head = document.createElement('th');
+            let name = document.createElement('td');
+            let points = document.createElement('td');
+            let wins = document.createElement('td');
+            let losses = document.createElement('td');
+            let streak = document.createElement('td');
+
+            let control = document.createElement('td');
+            let winButton = document.createElement('button');
+            let looseButton = document.createElement('button');
+            winButton.classList.add('btn', 'btn-success');
+            winButton.textContent = "Won";
+            looseButton.classList.add('btn', 'btn-danger');
+            looseButton.textContent = "Lost";
+            control.appendChild(winButton);
+            control.appendChild(looseButton);
+
+            div.classList.add("playerRow");
+            head.textContent = counter++;
+            name.textContent = entry.data().name;
+            points.textContent = entry.data().points;
+            wins.textContent = entry.data().wins;
+            losses.textContent = entry.data().losses;
+            streak.textContent = entry.data().currentStreak;
+
+            wins.classList.add('d-none', 'd-sm-table-cell');
+            losses.classList.add('d-none', 'd-sm-table-cell');
+            streak.classList.add('d-none', 'd-md-table-cell');
+
+            row.appendChild(head);
+            row.appendChild(name);
+            row.appendChild(points);
+            row.appendChild(wins);
+            row.appendChild(losses);
+            row.appendChild(streak);
+            row.appendChild(control);
+
+            mainTable.appendChild(row);
+        });
+    });
 });
 
-const form = document.querySelector("#addPlayerForm")
+const addPlayerForm = document.querySelector("#addPlayerForm");
 
-form.addEventListener('submit', (e) =>{
-    alert("hello");
+addPlayerForm.addEventListener('submit', (e) =>{
     e.preventDefault();
-    firebase.firestore().collection("Players").doc(form.name.value).set({
-        name: form.name.value,
+    firebase.firestore().collection("Players").doc(addPlayerForm.name.value).set({
+        name: addPlayerForm.name.value,
         points: 1500,
         wins: 0,
         losses: 0,
@@ -38,28 +81,17 @@ form.addEventListener('submit', (e) =>{
         console.error("Error writing document: ", error);
     });
 
-    form.name.value = '';
+    addPlayerForm.name.value = '';
 });
 
-function googleLogin(){
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(result =>{
-        
-        const user = result.user;
-        console.log(user.displayName);
-    }).catch(console.log)
-}
+const loginForm = document.querySelector("#loginForm");
 
-function passwordLogin(){
-    var password = prompt("Enter the password:");
-    firebase.auth().signInWithEmailAndPassword('loginuser@tablefootball.com', password).catch(function(error) {
+loginForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    firebase.auth().signInWithEmailAndPassword('loginuser@tablefootball.com', loginForm.password.value).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
     });
-}
-
-function addData(){
-
-}
+});
