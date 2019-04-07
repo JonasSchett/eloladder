40d7c5b8b0377dc663fb6646e7ce31d25d118528
Jonas Schett <jonas.schett@gmail.com>
2019-04-07 17:12:26 +0100

    Added doubles functionality

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,187 @@ admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+
+export const calculateDoubleMatch = functions.firestore.document('GameRequestsDoubles/{userId}').onCreate((snap,context) =>{
+  const snapData = snap.data();
+  if(snapData === undefined)
+  {
+      return null;
+  }
+
+  class Player {
+    name:string;
+    won:boolean;
+    points:number;
+    wins:number;
+    losses:number;
+    streak:number;
+    maxStreak:number;
+    constructor(name:string, won:boolean)
+    {
+      this.name = name;
+      this.won = won;
+      this.points = 0;
+      this.wins = 0;
+      this.losses = 0;
+      this.streak = 0;
+      this.maxStreak = 0;
+    }
+  }
+
+  let n = 0;
+  let k = 0;
+  let gamesPlayed = 0;
+  let decayCalculationFactor = 100;
+  let decayCutoffGames = 100;
+  let decayPoints = 0;
+
+  const winner1 = snapData.winner1;
+  const winner2 = snapData.winner2;
+  const loser1 = snapData.loser1;
+  const loser2 = snapData.loser2;
+
+  const losers = [new Player(loser1,false),new Player(loser2,false)];
+  const winners = [new Player(winner1,true),new Player(winner2,true)];
+  const allPlayers:Player[] = [];
+  allPlayers.push(...losers);
+  allPlayers.push(...winners);
+
+  let winningTeamPoints = 0;
+  let losingTeamPoints = 0;
+
+  const players = admin.firestore().collection('Players');
+  const info = admin.firestore().collection('information');
+  //var winningPoints = 0;
+  const gameInfo = info.doc('gameInformation');
+
+  const promises = [];
+
+  console.log("Doubles beginning log:");
+  for(const player of allPlayers)
+  {
+    const currentPlayer = players.doc(player.name);
+    promises.push(currentPlayer.get()
+    .catch(err => console.log(err))
+    .then(doc => {
+      if(doc !== undefined){
+        const data = doc.data();
+        if(data !== undefined)
+        {
+          if(player.won)
+          {
+            player.points = data.points;
+            winningTeamPoints += player.points;
+            player.wins = data.wins + 1;
+            player.losses = data.losses;
+            player.streak = data.currentStreak + 1;
+            player.maxStreak = player.streak > data.maxStreak ? player.streak : data.maxStreak;
+          }
+          else
+          {
+            player.points = data.points;
+            losingTeamPoints += player.points;
+            player.wins = data.wins;
+            player.losses = data.losses + 1;
+            player.streak = 0;
+            player.maxStreak = data.maxStreak;
+          }
+          console.log("Player: " + player.name + " Points: "+ player.points + " Won: " + player.won);
+        }
+      }
+    })
+    .catch(err => console.log(err)));
+  }
+
+  // get general game info from database
+  promises.push(gameInfo.get()
+    .catch(err => console.log(err))
+    .then(doc =>{
+      if(doc !== undefined){
+        const data = doc.data();
+        if(data !== undefined)
+        {
+          n = data.n;
+          k = data.k;
+          gamesPlayed = data.gamesPlayed;
+          decayCalculationFactor = data.decayCalculationFactor;
+          decayCutoffGames = data.decayCutoffGames;
+          decayPoints = data.decayPoints;
+          console.log("DecayFactor: "+ decayCalculationFactor+ " cutoffgames: " + decayCutoffGames + " decayPoints: " + decayPoints);
+        }
+      }
+    })
+    .catch(err => console.log(err))
+  );
+
+  Promise.all(promises)
+  .catch(err => console.log(err))
+  .then((e) =>{
+
+    gamesPlayed = gamesPlayed + 1;
+    const x  = winningTeamPoints - losingTeamPoints;
+    const exponent = -(x/n);
+    const winnerExpected = 1/(1+Math.pow(10,exponent));
+    const loserExpected = 1-winnerExpected;
+    const winnerAddition = Math.round(k * (1-winnerExpected));
+    const loserDeduction = Math.round(k * (0-loserExpected));
+
+    console.log("Winneraddition: "+ winnerAddition+" LoserDeduction: "+loserDeduction);
+
+    // winner addition and loser deduction will be applied to the players here
+    // each player will gain/lose the amount of points calculated.
+    // This effectively gives a doubles match the same score per player as a singles match.
+    // the points are also normalised accross the teams
+    // the person with more points pays more in case of a loss
+    // the person with more gains less in case of a win
+
+    // Below is some logging of the split among winner and loser
+    const winner1Addition = Math.round(winnerAddition * 2 * (winners[1].points / winningTeamPoints));
+    const winner2Addition = Math.round(winnerAddition * 2 * (winners[0].points / winningTeamPoints));
+    const loser1Addition = Math.round(loserDeduction * 2 * (losers[1].points / losingTeamPoints));
+    const loser2Addition = Math.round(loserDeduction * 2 * (losers[0].points / losingTeamPoints));
+    console.log("Winner split: Winner1: " + winner1Addition + " Winner2: "+ winner2Addition);
+    console.log("Loser split: Loser1: " + loser1Addition + " Loser2: "+ loser2Addition);
+    winners[0].points += winner1Addition;
+    winners[1].points += winner2Addition;
+    losers[0].points += loser1Addition;
+    losers[1].points += loser2Addition;
+
+    console.log("Doubles endning log:");
+    const innerPromises = [];
+    for(const player of allPlayers)
+    {
+      const currentPlayer = players.doc(player.name);
+      
+      console.log("Player: " + player.name + " Points: "+ player.points + " Won: " + player.won);
+      innerPromises.push(currentPlayer.update({
+        points: player.points,
+        wins: player.wins,
+        notPlayedFor: 0,
+        currentStreak: player.streak, 
+        maxStreak : player.maxStreak
+      })
+      .catch(err => console.log(err)));
+    }
+
+    innerPromises.push(gameInfo.update({
+        gamesPlayed: gamesPlayed,
+    })
+    .catch(err => console.log(err)));
+
+    //calculate decay
+    decayPreparation(innerPromises,players,gamesPlayed,decayCalculationFactor,decayCutoffGames,decayPoints);
+  })
+  .catch(err => console.log(err));
+  return snap.ref.set({
+    winner1:winner1,
+    winner2:winner2,
+    loser1:loser1,
+    loser2:loser2,
+    info:"Game was calculated"
+  }, {merge: true});
+});
+
 export const playerAdded = functions.firestore.document('PlayerAddRequests/{userId}').onCreate((snap,context) =>{
   const snapData = snap.data();
   if(snapData === undefined)
@@ -149,8 +330,13 @@ export const scriptUpdated = functions.firestore.document('GameRequests/{userId}
     const exponent = -(x/n);
     const winnerExpected = 1/(1+Math.pow(10,exponent));
     const loserExpected = 1-winnerExpected;
-    winningPoints = Math.round(winningPoints + k * (1-winnerExpected));
-    losingPoints = Math.round(losingPoints + k * (0-loserExpected));
+    const winnerAddition = Math.round(k * (1-winnerExpected));
+    const loserDeduction = Math.round(k * (0-loserExpected));
+
+    console.log("Winneraddition: "+ winnerAddition+" LoserDeduction: "+loserDeduction);
+
+    winningPoints = winningPoints + winnerAddition;
+    losingPoints = losingPoints + loserDeduction;
     winnerStreak += 1;
     winnerMaxStreak = winnerStreak > winnerMaxStreak ? winnerStreak : winnerMaxStreak;
 
@@ -187,34 +373,14 @@ export const scriptUpdated = functions.firestore.document('GameRequests/{userId}
     //update not played for value of players
     // we need to loop through every player,
     // count up and update our games
-    Promise.all(innerPromises)
-    .then(function(){
-      players.get()
-      .then(allPlayers =>{
-        allPlayers.forEach(player =>{
-            players.doc(player.data().name)
-            .update({
-                notPlayedFor: player.data().notPlayedFor + 1
-            })
-            .catch(err => console.log(err))
-        });
-      })
-      .catch(err => console.log(err))
-      .then(function(){
-          if(gamesPlayed % decayCalculationFactor === 0){
-              calculateDecay(players, decayCutoffGames, decayPoints);
-          }
-      })
-      .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
+    decayPreparation(innerPromises,players,gamesPlayed,decayCalculationFactor,decayCutoffGames,decayPoints);
   })
   .catch(err => console.log(err));
   
   return snap.ref.set({
   winner: winner,
   loser:loser,
-  read:"This game was calculated"
+  info:"Game was calculated"
   }, {merge: true});
 });
 
@@ -353,14 +519,35 @@ function calculateDecay(players : FirebaseFirestore.CollectionReference, decayCu
   .catch(err => console.log(err));
 }
 
-
-// playerToAdd.set({
-//   name: name,
-//   points: 1500,
-//   wins: 0,
-//   losses: 0,
-//   currentStreak: 0,
-//   maxStreak: 0,
-//   lastOpponent: '',
-//   notPlayedFor: 0
-// })
+function decayPreparation(promises: Promise<void | FirebaseFirestore.WriteResult>[], 
+                          players:FirebaseFirestore.CollectionReference, 
+                          gamesPlayed:number, 
+                          decayCalculationFactor:number,
+                          decayCutoffGames:number,
+                          decayPoints:number)
+{
+  Promise.all(promises)
+    .then(function(){
+      let totalScore = 0;
+      players.get()
+      .then(allPlayers =>{
+        allPlayers.forEach(player =>{
+          totalScore += player.data().points;
+          players.doc(player.data().name)
+          .update({
+              notPlayedFor: player.data().notPlayedFor + 1
+          })
+          .catch(err => console.log(err))
+        });
+      })
+      .catch(err => console.log(err))
+      .then(function(){
+          console.log("Current total score: " + totalScore);
+          if(gamesPlayed % decayCalculationFactor === 0){
+              calculateDecay(players, decayCutoffGames, decayPoints);
+          }
+      })
+      .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+}
--- public/app.js
+++ public/app.js
@@ -1,7 +1,5 @@
 var doubles = false;
 
-var currentWinner = '';
-var currentLoser = '';
 var currentWinners = [];
 var currentLosers = [];
 
@@ -143,192 +141,104 @@ function checkGame(winner, loser){
     return false;
 }
 
-function checkGameDoubles(winner1, winner2, loser1, loser2)
+function checkGameDoubles(winners, losers)
 {
-
-    // get db information
-    var db = firebase.firestore();
-    var players = db.collection("Players");
-    var gameInfo = db.collection("information").doc("gameInformation");
-
-    // get a list of players in which each player can be in the list only once
-    var playerList = [winner1, winner2, loser1, loser2].filter(distinct);
-
-    
-
-    // return if the player list does not contain 4 distinct players
-    if(playerList.length < 4)
+    // make sure last two players are in array
+    while(winners.length > 2)
     {
-        return;
+        winners.shift();
     }
 
-    var playerInfoList = [
-        {name:winner1, won: true},
-        {name:winner2, won: true},
-        {name:loser1, won: false},
-        {name:loser2, won: false},
-    ]
-
-    var promises = [];
-
-    for(var i = 0; i < playerInfoList.length; i++)
+    while(losers.length > 2)
     {
-        var currentPlayer = players.doc(playerInfoList[i].name);
-        console.log(currentPlayer);
-        promises.push(currentPlayer.get().then(doc => {
-            if(doc.exists){
-                console.log(doc.data().points);
-                currentPlayer.points = doc.data().points;
-                currentPlayer.wins = doc.data().wins + 1;
-                currentPlayer.lastOpponent = doc.data().lastOpponent;
-                currentPlayer.currentStreak = doc.data().currentStreak;
-                currentPlayer.maxStreak = doc.data().maxStreak;
-            }
-            else{
-                alert("asd");
-            }
-        }));
+        losers.shift();
     }
-    
-    
-    // get general game info from database
-    promises.push(gameInfo.get().then(doc =>{
-        if(doc.exists){
-            n = doc.data().n;
-            k = doc.data().k;
-            gamesPlayed = doc.data().gamesPlayed;
-            decayCalculationFactor = doc.data().decayCalculationFactor;
-        }
-    }));
 
-    Promise.all(promises).then((e) => {
-        console.log(playerInfoList); 
-    });
-}
+    console.log(losers);
+    console.log(winners);
 
+    var allPlayers = [];
+    allPlayers.push(...winners);
+    allPlayers.push(...losers);
+    if(new Set(allPlayers).size !== 4 || winners.length !== 2 || losers.length !== 2)
+    {
+        console.log("illegal game-setup");
+        return false;
+    }
 
-function calculateDecay(){
-    var db = firebase.firestore();
-    var players = db.collection("Players");
-    var gameInfo = db.collection("information").doc("gameInformation");
-    var decayCutoffGames = 0;
-    var decayPoints = 0; 
-    var decayPlayersArray = [];
-    var gainingPlayersArray = [];
-
-    gameInfo.get().then(doc =>{
-        if(doc.exists){
-            decayCutoffGames = doc.data().decayCutoffGames;
-            decayPoints = doc.data().decayPoints;
-        }
-    }).then((e) =>{
-        decayPlayers = players.where("notPlayedFor", ">", decayCutoffGames);
-        gainingPlayers = players.where("notPlayedFor", "<=", decayCutoffGames);
-
-        const promises = [];
-        promises.push(decayPlayers.get().then(players =>{
-            players.forEach(player =>{
-                // console.log("decay: " + player.data().name);
-                decayPlayersArray.push({
-                    name: player.data().name,
-                    notPlayedFor: player.data().notPlayedFor,
-                    currentPoints: player.data().points,
-                });
-            });
-        }).catch(error =>{
-            console.log(error);
-        }));
-
-        promises.push(gainingPlayers.get().then(players =>{
-            players.forEach(player =>{
-                // console.log("gain: " + player.data().name);
-                gainingPlayersArray.push({
-                    name: player.data().name,
-                    notPlayedForInverted: decayCutoffGames - player.data().notPlayedFor,
-                    currentPoints: player.data().points,
-                });
-            });
-        }).catch(error =>{
-            console.log(error);
-        }));
-
-        Promise.all(promises).then((e) =>{
-
-            if(decayPlayersArray.length == 0 || decayPoints == 0)
-            {
-                console.log("aborting decay calculation");
-                return
-            }
-
-            console.log("calculating decay points");
-            
-            // calculate all losses
-            var totalNotPlayedFor = 0;
-            var totalDecay = decayPoints * decayPlayersArray.length;
-            var totalActualDecay = 0;
-            for(var i = 0; i < decayPlayersArray.length; i ++)
-            {
-                totalNotPlayedFor += decayPlayersArray[i].notPlayedFor;
-            }
-
-            // console.log("Total decay not played for: " + totalNotPlayedFor);
-
-            for(var i = 0; i < decayPlayersArray.length; i ++)
-            {
-                var percentage = decayPlayersArray[i].notPlayedFor/totalNotPlayedFor;
-                // console.log(decayPlayersArray[i].name + ": " + percentage);
-                var decayLosses = Math.round(percentage * totalDecay);
-                totalActualDecay += decayLosses;
-                decayPlayersArray[i].currentPoints -= decayLosses;
-                //update database
-                var currentPlayer = players.doc(decayPlayersArray[i].name);
-                currentPlayer.update({
-                    points: decayPlayersArray[i].currentPoints
-                });
-            }
-
-            // calculate all gains
-            var totalInvertedNotPlayedFor = 0;
-            var totalGains = totalActualDecay;
-            var totalActualGains = 0;
-
-            // console.log("Total gains: " + totalGains);
-
-            for(var i = 0; i < gainingPlayersArray.length; i ++)
-            {
-                totalInvertedNotPlayedFor += gainingPlayersArray[i].notPlayedForInverted; 
-            }
-
-            // console.log("Total inverted not played for: " + totalInvertedNotPlayedFor);
-
-            for(var i = 0; i < gainingPlayersArray.length; i ++)
-            {
-                percentage = gainingPlayersArray[i].notPlayedForInverted/totalInvertedNotPlayedFor;
-                decayGains = Math.round(percentage * totalGains);
-                totalActualGains += decayGains;
-                gainingPlayersArray[i].currentPoints += decayGains;
-                // console.log(gainingPlayersArray[i].name + ": " + percentage);
-                //update database
-                var currentPlayer = players.doc(gainingPlayersArray[i].name);
-                currentPlayer.update({
-                    points: gainingPlayersArray[i].currentPoints
-                });
-            }
-
-            // check if there are no runding errors, and if there are, adjust for them
-            if(totalActualGains != totalActualDecay){
-                
-                var difference = totalActualDecay - totalActualGains;
-                console.log("There is a difference in gains and decay, adjusting");
-                // give difference to random player
-                var luckyPick = gainingPlayersArray[0];
-
-                players.doc(luckyPick.name).update({
-                    points: luckyPick.currentPoints + difference
-                });
-            }
+    if(confirm("Did " + winners[0] + " & "+ winners[1] + " defeat " + losers[0] + " & " + losers[1] + "?"))
+    {
+        var db = firebase.firestore();
+        db.collection("GameRequestsDoubles").add({
+            winner1: winners.pop(),
+            winner2: winners.pop(),
+            loser1: losers.pop(),
+            loser2: losers.pop(),
         });
-    });
+        return true;
+    }
+
+
+    
+
+    if((winner != undefined) && (loser != undefined) && (winner != loser) && confirm("Did " + winner + " defeat " + loser + "?"))
+    {
+        var db = firebase.firestore();
+        var players = db.collection("Players");
+        var gameInfo = db.collection("information").doc("gameInformation");
+        var winningPlayer1 = players.doc(winner);
+        var losingPlayer1 = players.doc(loser);
+        //points of winning and losing player
+
+        //last opponents of winner/loser
+        var winnerLastOpponent = '';
+        var loserLastOpponent = '';
+
+        // get results from the database and wait for them to return promises
+        const promises = [];
+
+        //get winning player points
+        promises.push(winningPlayer1.get().then(doc => {
+            if(doc.exists){
+                winningPoints1 = doc.data().points;
+                winningWins1 = doc.data().wins + 1;
+                winnerLastOpponent = doc.data().lastOpponent;
+                winnerStreak = doc.data().currentStreak;
+                winnerMaxStreak = doc.data().maxStreak;
+            }
+        }));
+
+        //get losing player points
+        promises.push(losingPlayer1.get().then(doc => {
+            if(doc.exists){
+                losingPoints1 = doc.data().points;
+                losingLosses1 = doc.data().losses + 1;
+                loserLastOpponent = doc.data().lastOpponent;
+            }
+        }));
+
+        // after all promises have been received we can continue with the
+        // actual calculation of the program
+        Promise.all(promises).then((e) => {
+            //check if the game is actually valid from the data gathered:
+            // if it is invalid we return here, should a player manage to get past this,
+            // google functions will check for this again
+            if(loserLastOpponent == winner || winnerLastOpponent == loser)
+            {
+                alert('Each player has to play at least someone else, before playing again');
+                return;
+            }
+
+            // Send gamerequest to database
+            // calculation of scores will happen in google functions
+            db.collection("GameRequests").add({
+                winner: winner,
+                loser: loser
+            });
+        });
+        return true;
+    }
+    return false;
 }
 
 const singlesButton = document.querySelector("#singles");
@@ -444,18 +354,19 @@ function addGameParticipant(player, won)
     if(won)
     {
         currentWinners.push(player);
-        currentWinner = player;
     }
     else
     {
         currentLosers.push(player);
-        currentLoser = player;
     }
 
     if(doubles)
     {
-
-        //check for doubles match
+        if(checkGameDoubles(currentWinners, currentLosers))
+        {
+            currentWinners.splice(0, currentWinners.length);
+            currentLosers.splice(0, currentLosers.length);
+        }
     }
     else
     {
